46d70100499ec3a0e8442e52ecd4590980d9b0b3
Pavan <pavan561981@gmail.com>
2025-11-13 10:38:14 +0530

    Created Layouts files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>This is Portfolio of Pa1</h1>
-        </div>
+     <Navbar/>
+     {/* <Hero /> */}
     </>
   )
 }
